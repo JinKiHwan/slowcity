f7141c27b678482d78ac4d3a093733f99b1aaa69
Kihwan <eljsh95@naver.com>
2024-03-20 16:55:42 +0900

    모바일 접근 막기

--- resources/js/slowcity.js
+++ resources/js/slowcity.js
@@ -65,6 +65,20 @@ gsap.to(introLogo, {
 
 /* //gsap */
 $(document).ready(function () {
+  const isMobile = (() => {
+    const { userAgent, maxTouchPoints } = window.navigator;
+    const isMac = /Macintosh/i.test(userAgent);
+    if (isMac && maxTouchPoints > 0) return true;
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobi|mobi/i.test(
+      userAgent
+    );
+  })();
+
+  if (isMobile) {
+    $('section').hide();
+    $('.mobile-blur').show();
+  }
+
   $('.intro-logo').hover(function () {
     $('.intro__wrap').stop().fadeToggle();
   });
